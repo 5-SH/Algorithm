d59ada951a1c029b263b65c9eee67a3c8cecc035
OSH <llll_lll_lll@naver.com>
2020-05-05 21:36:00 +0900

    14890 에러 해결

--- algo_14890/Main.java
+++ algo_14890/Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -18,27 +17,25 @@ public class Main {
       }
     }
 
-    boolean[] visited = new boolean[N];
-
+    boolean[] installed = new boolean[N];
     // 세로
     for (int x = 0; x < N; x++) {
-      boolean check = true;
+      boolean hasWay = true;
       for (int i = 0; i < N; i++) {
-        visited[i] = false;
+        // 경사로가 설치되었는지 확인
+        installed[i] = false;
       }
       for (int y = 0; y < N; y++) {
-        if (check == false) {
+        if (hasWay == false) {
           break;
         }
         if (y == N - 1) {
           way++;
-          // System.out.println("---------------------------세로" + x + ", " + way);
           break;
         }
 
         int currHeight = map[y][x];
         int nextHeight = map[y + 1][x];
-
         if (nextHeight == currHeight) {
           continue;
         }
@@ -47,148 +44,99 @@ public class Main {
           int diff = currHeight - nextHeight;
           if (diff == 1) {
             for (int l = 1; l <= L; l++) {
-              if (y + l < N && currHeight - map[y + l][x] == 1 && (visited[y + l] == false)) {
+              if (y + l < N && currHeight - map[y + l][x] == 1 && (installed[y + l] == false)) {
                 if (l == L) {
                   for (int i = 1; i <= L; i++) {
-                    // map[y + i][x] += 10;
-                    visited[y + i] = true;
+                    installed[y + i] = true;
                   }
                   y += L - 1;
                 }
               } else {
-                check = false;
+                hasWay = false;
                 break;
               }
             }
           }
-
           // 올라가는 판단
           else if (diff == -1) {
             for (int l = 0; l < L; l++) {
-              if (y - l >= 0 && nextHeight - map[y - l][x] == 1 && (visited[y - l] == false)) {
+              if (y - l >= 0 && nextHeight - map[y - l][x] == 1 && (installed[y - l] == false)) {
                 if (l == L - 1) {
                   for (int i = 0; i < L; i++) {
-                    // map[y - i][x] += 10;
-                    visited[y + i] = true;
+                    installed[y - i] = true;
                   }
                 }
               } else {
-                check = false;
+                hasWay = false;
                 break;
               }
             }
-          }
-
-          else {
-            check = false;
+          } else {
+            hasWay = false;
           }
         }
       }
     }
 
     // 가로
-
     for (int y = 0; y < N; y++) {
-      boolean check = true;
+      boolean hasWay = true;
       for (int i = 0; i < N; i++) {
-        visited[i] = false;
+        installed[i] = false;
       }
-      // System.out.println("-----------------------------------" + y);
       for (int x = 0; x < N; x++) {
-        if (check == false) {
+        if (hasWay == false) {
           break;
         }
         if (x == N - 1) {
-          // System.out.println("count: " + y);
           way++;
-          // System.out.println("---------------------------가로" + y + ", " + way);
           break;
         }
 
         int currHeight = map[y][x];
         int nextHeight = map[y][x + 1];
-
         if (nextHeight == currHeight) {
           continue;
         }
         // 내려가는 판단
         else {
           int diff = currHeight - nextHeight;
-
-          // if (y == 2) {
-
-          // System.out.println("diff: " + diff);
-          // }
-
           if (diff == 1) {
             for (int l = 1; l <= L; l++) {
-
-              if (y == 2) {
-
-                // System.out
-                // .println("down if: " + (x + l < N) + (currHeight - map[y][x + l] == 1) +
-                // (visited[x + l] == false));
-              }
-
-              if (x + l < N && currHeight - map[y][x + l] == 1 && (visited[x + l] == false)) {
+              if (x + l < N && currHeight - map[y][x + l] == 1 && (installed[x + l] == false)) {
                 if (l == L) {
                   for (int i = 1; i <= L; i++) {
-                    visited[x + i] = true;
-                    // map[y][x + i] += 10;
+                    installed[x + i] = true;
                   }
                   x += L - 1;
                 }
               } else {
-                check = false;
+                hasWay = false;
                 break;
               }
             }
           }
-
           // 올라가는 판단
           else if (diff == -1) {
             for (int l = 0; l < L; l++) {
-
-              // if (y == 2) {
-
-              // System.out
-              // .println("up if: " + (x - l >= 0 && nextHeight - map[y][x - l] == 1 &&
-              // (visited[x - l] == false)));
-
-              // System.out.println("x + l: " + (x + l));
-              // for (int j = 0; j < N; j++) {
-              // System.out.print(visited[j] + ", ");
-              // }
-              // System.out.println();
-              // }
-
-              if (x - l >= 0 && nextHeight - map[y][x - l] == 1 && (visited[x - l] == false)) {
+              if (x - l >= 0 && nextHeight - map[y][x - l] == 1 && (installed[x - l] == false)) {
                 if (l == L - 1) {
                   for (int i = 0; i < L; i++) {
-                    visited[x + i] = true;
-                    // map[y][x - i] += 10;
+                    installed[x - i] = true;
                   }
                 }
               } else {
-                check = false;
+                hasWay = false;
                 break;
               }
             }
-          }
-
-          else {
-            check = false;
+          } else {
+            hasWay = false;
           }
         }
       }
     }
 
     System.out.println(way);
-    // for (int y = 0; y < N; y++) {
-    // for (int x = 0; x < N; x++) {
-    // System.out.print(map[y][x]);
-    // }
-    // System.out.println();
-    // }
   }
 }
