f21709365d7c85cdb57c8e0d72ab5f8338543565
OSH <llll_lll_lll@naver.com>
2021-04-26 20:44:16 +0900

    알고리즘 다시 시작

---
+++ java/src/직사각형_별찍기/Main.java
@@ -0,0 +1,20 @@
+package 직사각형_별찍기;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    for (int i = 1; i <= a * b; i++) {
+      if ((i % a) == 0 && (i != (a * b))) {
+        System.out.print('*');
+        System.out.println();
+      } else {
+        System.out.print('*');
+      }
+    }
+  }
+}
