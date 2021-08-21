e2382696f8a81dfa54044fba4ade859fc7c24891
OSH <llll_lll_lll@naver.com>
2021-08-22 01:15:45 +0900

    codility lesson7 Brackets 시간복잡도 해결

--- java/src/codility/Brackets/Main.java
+++ java/src/codility/Brackets/Main.java
@@ -27,6 +27,31 @@ public class Main {
     return 0;
   }
 
+  // string equals 는 hashcode, equal 검사해서 시간이 느릴 수 있다.
+  static public int solution2(String S) {
+    // write your code in Java SE 8
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < S.length(); i++) {
+      char c = S.charAt(i);
+      if (c == '(' || c == '{' || c == '[') {
+        stack.push(c);
+      } else {
+        if (stack.isEmpty()) return 0;
+        char chk = stack.pop();
+        if (c == ')' && chk != '('){
+          return 0;
+        } else if (c == '}' && chk != '{') {
+          return 0;
+        } else if (c == ']' && chk != '[') {
+          return 0;
+        }
+      }
+    }
+
+    if (stack.isEmpty()) return 1;
+    return 0;
+  }
+
   public static void main(String[] args) {
 //    String S = "{[()()]}"; // 1
 //    String S = "[)"; // 0
@@ -34,6 +59,6 @@ public class Main {
 //    String S = ""; // 1
 //    String S = "}"; // 0
     String S = "{{{[[]]}}}{}{}{}{}{}{}{}{}{}[][][][][][][][]()()()()()()()"; // 1
-    System.out.println(solution(S));
+    System.out.println(solution2(S));
   }
 }
