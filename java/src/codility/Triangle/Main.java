2d563dab0d668b509cb4e5700e066b14971e2b7f
OSH <llll_lll_lll@naver.com>
2021-08-22 00:29:06 +0900

    codility lesson6 Tirangle 시간복잡도 수정

--- java/src/codility/Triangle/Main.java
+++ java/src/codility/Triangle/Main.java
@@ -29,6 +29,8 @@ public class Main {
     Collections.sort(list);
 
     long P, Q, R;
+    // 정렬된 상태에서 P + R > Q, Q + R > P 항상 만족
+    // P + Q > R 만 확인
     ArrayList<ArrayList<Long>> checkList = new ArrayList<ArrayList<Long>>();
     for (int i = 0; i < list.size() - 2; i++) {
       P = Long.valueOf(list.get(i));
@@ -60,11 +62,41 @@ public class Main {
     return 0;
   }
 
+  static public int solution2(int[] A) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < A.length; i++) {
+      if (A[i] > 0) {
+        list.add(A[i]);
+      }
+    }
+    Collections.sort(list);
+
+    long P, Q, R;
+    // 정렬된 상태에서 P + R > Q, Q + R > P 항상 만족
+    // P + Q > R 만 확인
+    for (int i = 0; i < list.size() - 2; i++) {
+      P = Long.valueOf(list.get(i));
+      for (int j = i + 1; j < list.size() - 1; j++) {
+        Q = Long.valueOf(list.get(j));
+        for (int k = j + 1; k < list.size(); k++) {
+          R = Long.valueOf(list.get(k));
+          if (P + Q > R) {
+            return 1;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+
+    return 0;
+  }
+
   public static void main(String[] args) {
 //    int[] A = {10,2,5,1,8,20};
 //    int[] A = {10,2,5,1,8,20,-10,-1,0,-50};
-//    int[] A = {10,50,5,1};
-    int[] A = {2147483647, 2147483647, 2147483647, 10,2,5,1,8,20,-10,-1,0,-50};
+    int[] A = {10,50,5,1};
+//    int[] A = {2147483647, 2147483647, 2147483647, 10,2,5,1,8,20,-10,-1,0,-50};
     System.out.println(solution(A));
   }
 }
