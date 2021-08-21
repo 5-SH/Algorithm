261133b6f63445d76a9d432283797db0d62e0f58
OSH <llll_lll_lll@naver.com>
2021-08-22 00:21:20 +0900

    codility lesson6 Triangle

---
+++ java/src/codility/Triangle/Main.java
@@ -0,0 +1,70 @@
+package codility.Triangle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+  public static void print(int[] arr) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("(");
+    for (int i = 0; i < arr.length; i++) {
+      builder.append(arr[i]);
+      if (i != arr.length - 1) {
+        builder.append(", ");
+      }
+    }
+    builder.append(")");
+    System.out.println(builder.toString());
+  }
+
+  static public int solution(int[] A) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < A.length; i++) {
+      if (A[i] > 0) {
+        list.add(A[i]);
+      }
+    }
+    Collections.sort(list);
+
+    long P, Q, R;
+    ArrayList<ArrayList<Long>> checkList = new ArrayList<ArrayList<Long>>();
+    for (int i = 0; i < list.size() - 2; i++) {
+      P = Long.valueOf(list.get(i));
+      for (int j = i + 1; j < list.size() - 1; j++) {
+        Q = Long.valueOf(list.get(j));
+        for (int k = j + 1; k < list.size(); k++) {
+          R = Long.valueOf(list.get(k));
+          if (P + Q <= R) {
+            break;
+          }
+          ArrayList<Long> l = new ArrayList<>();
+          l.add(P);
+          l.add(Q);
+          l.add(R);
+          checkList.add(l);
+        }
+      }
+    }
+
+    for (ArrayList<Long> l : checkList) {
+      long vP = l.get(0);
+      long vQ = l.get(1);
+      long vR = l.get(2);
+
+      if ((vP + vQ > vR) && (vQ + vR > vP) && (vR + vP > vQ)) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  public static void main(String[] args) {
+//    int[] A = {10,2,5,1,8,20};
+//    int[] A = {10,2,5,1,8,20,-10,-1,0,-50};
+//    int[] A = {10,50,5,1};
+    int[] A = {2147483647, 2147483647, 2147483647, 10,2,5,1,8,20,-10,-1,0,-50};
+    System.out.println(solution(A));
+  }
+}
