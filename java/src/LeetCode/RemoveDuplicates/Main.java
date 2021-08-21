dc5a731baffb91a746b8fadf9a4e4e4a7324f179
OSH <llll_lll_lll@naver.com>
2021-08-21 23:00:32 +0900

    주석 추가

--- java/src/LeetCode/RemoveDuplicates/Main.java
+++ java/src/LeetCode/RemoveDuplicates/Main.java
@@ -14,6 +14,7 @@ public class Main {
     System.out.println(builder.toString());
   }
 
+  // two pointer
   static public int removeDuplicates(int[] nums) {
     final int NaN = -7777;
     int chk = NaN;
